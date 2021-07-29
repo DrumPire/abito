3339c9dd212a3ae14a52e7159d963e5de71b1c0c
DrumPire <expresproject@gmail.com>
2021-07-29 19:24:36 +0300

    responsive

--- js/main.js
+++ js/main.js
@@ -4,10 +4,26 @@ var swiper = new Swiper(".mySwiper", {
   freeMode: true,
   watchSlidesVisibility: true,
   watchSlidesProgress: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 4,
+    }
+  }
 });
 var swiper2 = new Swiper(".mySwiper2", {
   spaceBetween: 10,
   thumbs: {
     swiper: swiper,
   },
+});
+
+const menuBtn = document.querySelector('.menu-button'),
+      menu = document.querySelector('.navbar-panel'),
+      closeMenu = document.querySelector('.close-menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.add('is-active');
+});
+closeMenu.addEventListener('click', () => {
+  menu.classList.remove('is-active');
 });
